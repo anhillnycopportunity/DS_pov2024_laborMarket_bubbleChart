06eb81afdee78a3792d10958da0c27a90aabd251
anhillnycopportunity <anhill@nycopportunity.nyc.gov>
2026-04-10 16:04:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -28,28 +28,110 @@ fetch('data.csv')
     }));
 
     Highcharts.chart('container', {
-      chart: {
+
+    chart: {
         type: 'bubble',
         plotBorderWidth: 1,
-        zoomType: 'xy'
-      },
+        zooming: {
+            type: 'xy'
+        }
+    },
 
-      title: {
-        text: 'Bubble Chart from CSV'
-      },
+    legend: {
+        enabled: false
+    },
 
-      xAxis: {
-        title: { text: 'X Axis' }
-      },
+    title: {
+        text: 'Test: Representation, industry, wages, job quality...'
+    },
 
-      yAxis: {
-        title: { text: 'Y Axis' }
-      },
+    subtitle: {
+        text: 'Source: <a href="http://www.euromonitor.com/">Euromonitor</a> and <a href="https://data.oecd.org/">OECD</a>'
+    },
 
-      tooltip: {
-        pointFormat: '<b>{point.name}</b><br>X: {point.x}, Y: {point.y}, Size: {point.z}'
-      },
+    accessibility: {
+        point: {
+            valueDescriptionFormat: '{index}. {point.name}, Ratio PT/FT: {point.x}, ' +
+                'Percent CW Median Income: {point.y}%, Count Workers: {point.z}%.'
+        }
+    },
 
-      series: series
-    });
-  });
+    xAxis: {
+        gridLineWidth: 1,
+        title: {
+            text: 'Ratio of Part-Time to Full-Time Workers'
+        },
+        labels: {
+            format: '{value}'
+        },
+        plotLines: [{
+            dashStyle: 'dot',
+            width: 2,
+            value: .5,
+            label: {
+                rotation: 0,
+                y: 50,
+                style: {
+                    fontStyle: 'italic'
+                },
+                text: 'Ratio Part-time Full-time'
+            },
+            zIndex: 3
+        }],
+        accessibility: {
+            rangeDescription: 'Range: tbd.'
+        }
+    },
+
+    yAxis: {
+        startOnTick: false,
+        endOnTick: false,
+        title: {
+            text: 'Percent Industry Median to Citywide Median Wage'
+        },
+        labels: {
+            format: '{value}%'
+        },
+        maxPadding: .1,
+        plotLines: [{
+            dashStyle: 'dot',
+            width: 2,
+            value: 100,
+            label: {
+                align: 'right',
+                style: {
+                    fontStyle: 'italic'
+                },
+                text: 'Equal to Citywide Median Wage',
+                x: 0.5
+            },
+            zIndex: 3
+        }],
+        accessibility: {
+            rangeDescription: 'Range: tbd.'
+        }
+    },
+
+    tooltip: {
+        useHTML: true,
+        headerFormat: '<table>',
+        pointFormat: '<tr><th colspan="2"><h3>{point.name}</h3></th></tr>' +
+            '<tr><th>Ratio PT/FT:</th><td>{point.x}</td></tr>' +
+            '<tr><th>Percent CW:</th><td>{point.y}%</td></tr>' +
+            '<tr><th>Workers (Count):</th><td>{point.z}</td></tr>',
+        footerFormat: '</table>',
+        followPointer: true
+    },
+
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: false,
+                format: '{point.name}'
+            }
+        }
+    },
+
+    series: series
+
+});
